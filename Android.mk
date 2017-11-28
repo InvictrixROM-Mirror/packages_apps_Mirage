cc5a5e80f8b03ccb658d6929bb428e89d5cd4249
Dan Cartier <nospamdan1@gmail.com>
2017-11-28 06:13:52 -0500

    Mirage: Fix compilation issues with proguard
    Change-Id: Idf78086c4d2cf9a1e8f4267331ae60a9bb865ef9

--- Android.mk
+++ Android.mk
@@ -14,6 +14,8 @@ LOCAL_SRC_FILES += $(call all-java-files-under, src)
 
 LOCAL_MODULE_TAGS := optional
 LOCAL_SDK_VERSION := current
+LOCAL_PACKAGE_NAME := Mirage
+LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v4 \
@@ -28,7 +30,5 @@ LOCAL_AAPT_FLAGS := --auto-add-overlay \
     --extra-packages android.support.v4 \
     --extra-packages android.support.v7.appcompat \
     --extra-packages android.support.design
-
-LOCAL_PACKAGE_NAME := Mirage
 
 include $(BUILD_PACKAGE)
