6388d0c405d80171381aa74da0353abe4dcbe523
CertifiedBlyndGuy <certifiedblyndguy@gmail.com>
2018-02-24 14:36:14 -0700

    Mirage: Initial consolidation for Invictrix
    * ParanoidPapers was used as a base due to its simplicity
    * I removed their walls as we will use our own
    * A huge thanks to them for the initial application

--- src/org/invictrix/Mirage/WallpaperActivity.java
+++ src/org/invictrix/Mirage/WallpaperActivity.java
@@ -7,7 +7,7 @@
  * or at https://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-package com.paranoid.ParanoidPapers;
+package org.invictrix.Mirage;
 
 import android.app.Activity;
 import android.app.AlertDialog;
